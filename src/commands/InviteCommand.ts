ffb5452b30bbdcaf8c2cea17cf8b20117302a9e6
kuimgame77 <76770335+kuimgame77@users.noreply.github.com>
2021-07-12 09:39:44 +0900

    Update InviteCommand.ts

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -14,7 +14,7 @@ export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
         message.channel.send(
             createEmbed("info")
-                .addField(`${this.client.user!.tag} - Invite Link`, `**[Click here to invite this bot](${await this.client.generateInvite({ permissions: 53857345 })})**`)
+                .addField(`${this.client.user!.tag} - 초대 링크`, `**[랑크](${await this.client.generateInvite({ permissions: 53857345 })})**`)
         ).catch(e => this.client.logger.error("PLAY_CMD_ERR:", e));
     }
 }
